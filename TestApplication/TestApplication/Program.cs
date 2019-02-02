33fd2d397ee6d5fdab861fc9d884b9efaa7848b4
Kirill Korzun <kirill.korzun.97@gmail.com>
2019-02-02 18:10:15 +0200

    Fixed severe production problem

--- TestApplication/TestApplication/Program.cs
+++ TestApplication/TestApplication/Program.cs
@@ -15,6 +15,7 @@ namespace TestApplication
             Console.WriteLine("Added a new release 2.0");
             Console.WriteLine("Added a new feature2 branch.");
             Console.WriteLine("HOTFIX 2.0.1");
+            Console.WriteLine("Some changes in Hotfix 2.0.1");
         }
     }
 }
